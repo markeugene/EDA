a81233c2b732758181e9d5ffdd0b80b6fa78d492
lasky <kovgunovsasha7@gmail.com>
2024-04-12 02:07:33 +0300

    removed unnessasary classes

--- order-server/src/main/java/com/example/eda/CreateOrder.java
+++
@@ -1,7 +0,0 @@
-package com.example.eda;
-
-import lombok.Builder;
-
-@Builder
-public record CreateOrder(String item, Integer quantity, Double totalCost, String orderDestination,String userId) {
-}
